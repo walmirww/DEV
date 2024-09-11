b87620d1b974e19fc590e1892bb190e35a9ac49e
Walmir Wachholz Wild <39489944+walmirww@users.noreply.github.com>
2024-09-11 10:56:24 -0300

    modulo

--- Walkthrough-Tutorial/webapp/controller/App.controller.js
+++ Walkthrough-Tutorial/webapp/controller/App.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function(Controller, MessageToast) {
     "use strict";
  
     return Controller.extend("sap.ui.demo.walkthrough.App", {
        onShowHello() {
           // show a native JavaScript alert
-          alert("Hello World from Controller");
+          //alert("Hello World from Controller");
+          MessageToast.show("Hello World");
        }
     });
  });
