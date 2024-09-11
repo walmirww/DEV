c732cd2293f40cd8b923489a506089cafb867ad6
Walmir Wachholz Wild <39489944+walmirww@users.noreply.github.com>
2024-09-11 11:42:42 -0300

    i18n

--- Walkthrough-Tutorial/webapp/controller/App.controller.js
+++ Walkthrough-Tutorial/webapp/controller/App.controller.js
@@ -1,14 +1,39 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function(Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+ ], function(Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
  
     return Controller.extend("sap.ui.demo.walkthrough.App", {
+
+      onInit() {
+         // set data model on view
+         let oData = {
+            recipient : {
+               name : "UI5"
+            }
+         };
+         let oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+
+         // set i18n model on view
+         let i18nModel = new ResourceModel({
+            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+         });
+         this.getView().setModel(i18nModel, "i18n");
+      },
+
        onShowHello() {
-          // show a native JavaScript alert
-          //alert("Hello World from Controller");
-          MessageToast.show("Hello World");
+          // read msg from i18n model
+         let oBundle = this.getView().getModel("i18n").getResourceBundle();
+         let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+         // show message
+         //MessageToast.show("Hello World");
+         MessageToast.show(sMsg);
        }
     });
  });
