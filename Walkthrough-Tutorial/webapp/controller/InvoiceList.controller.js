c082e79a1d120cb8477a9aba0a9e1a5c051f52fb
Walmir Wachholz Wild <39489944+walmirww@users.noreply.github.com>
2024-09-12 12:00:03 -0300

    Remote OData Service

--- Walkthrough-Tutorial/webapp/controller/InvoiceList.controller.js
+++ Walkthrough-Tutorial/webapp/controller/InvoiceList.controller.js
@@ -1,15 +1,33 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict"
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+        formatter: formatter,
         onInit: function () {
             let oViewModel = new JSONModel({
                 currency: "EUR"
             });
             this.getView().setModel(oViewModel, "view")
-        }
+        },
+
+		onFilterInvoices(oEvent) {
+			// build filter array
+			let aFilter = [];
+			let sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			let oList = this.byId("invoiceList");
+			let oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		}
     })
 })
