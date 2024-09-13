01c5c67aa990a9618188cb65bbd7aefdc64308a2
Walmir Wachholz Wild <39489944+walmirww@users.noreply.github.com>
2024-09-13 09:37:11 -0300

    Controls, responsiveness and Adaption

--- Walkthrough-Tutorial/webapp/component.js
+++ Walkthrough-Tutorial/webapp/component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/Device"
+], function (UIComponent, JSONModel, Device) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata : {
@@ -20,6 +21,11 @@ sap.ui.define([
             };
             let oModel = new JSONModel(oData);
             this.setModel(oModel);
+
+            // set device model
+            var oDeviceModel = new JSONModel(Device);
+            oDeviceModel.setDefaultBindingMode("OneWay");
+            this.setModel(oDeviceModel, "device");
 
             // create the views based on the url/hash
             this.getRouter().initialize();
--- Walkthrough-Tutorial/webapp/controller/Detail.controller.js
+++ Walkthrough-Tutorial/webapp/controller/Detail.controller.js
@@ -1,14 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], function (Controller, History) {
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, UIComponent, History, MessageToast, JSONModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
         onInit: function () {
+            var oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view")
+            
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
         _onObjectMatched: function (oEvent) {
+            this.byId("rating").reset();
             this.getView().bindElement({
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
@@ -25,6 +34,13 @@ sap.ui.define([
 				const oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo("overview", {}, true);
 			}
+		},
+        
+        onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 
     })
